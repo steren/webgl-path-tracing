313dc15b755eb5a50c25ccb74a860410c464fc66
Steren <steren.giannini@gmail.com>
2023-04-23 01:48:14 +0000

    extract pathTracer to function

--- webgl-path-tracing.js
+++ webgl-path-tracing.js
@@ -1003,10 +1003,9 @@ function tick(timeSinceStart) {
   ui.render();
 }
 
-window.onload = function() {
+function makePathTracer(canvas) {
   gl = null;
   error = document.getElementById('error');
-  canvas = document.getElementById('canvas');
   try { gl = canvas.getContext('experimental-webgl'); } catch(e) {}
 
   if(gl) {
@@ -1029,91 +1028,92 @@ window.onload = function() {
   } else {
     error.innerHTML = 'Your browser does not support WebGL.<br>Please see <a href="http://www.khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">Getting a WebGL Implementation</a>.';
   }
-};
-
-function elementPos(element) {
-  var x = 0, y = 0;
-  while(element.offsetParent) {
-    x += element.offsetLeft;
-    y += element.offsetTop;
-    element = element.offsetParent;
-  }
-  return { x: x, y: y };
-}
-
-function eventPos(event) {
-  return {
-    x: event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
-    y: event.clientY + document.body.scrollTop + document.documentElement.scrollTop
-  };
-}
-
-function canvasMousePos(event) {
-  var mousePos = eventPos(event);
-  var canvasPos = elementPos(canvas);
-  return {
-    x: mousePos.x - canvasPos.x,
-    y: mousePos.y - canvasPos.y
-  };
-}
-
-var mouseDown = false, oldX, oldY;
-
-document.onmousedown = function(event) {
-  var mouse = canvasMousePos(event);
-  oldX = mouse.x;
-  oldY = mouse.y;
-
-  if(mouse.x >= 0 && mouse.x < 512 && mouse.y >= 0 && mouse.y < 512) {
-    mouseDown = !ui.mouseDown(mouse.x, mouse.y);
-
-    // disable selection because dragging is used for rotating the camera and moving objects
-    return false;
+  
+  function elementPos(element) {
+    var x = 0, y = 0;
+    while(element.offsetParent) {
+      x += element.offsetLeft;
+      y += element.offsetTop;
+      element = element.offsetParent;
+    }
+    return { x: x, y: y };
   }
 
-  return true;
-};
+  function eventPos(event) {
+    return {
+      x: event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
+      y: event.clientY + document.body.scrollTop + document.documentElement.scrollTop
+    };
+  }
 
-document.onmousemove = function(event) {
-  var mouse = canvasMousePos(event);
+  function canvasMousePos(event) {
+    var mousePos = eventPos(event);
+    var canvasPos = elementPos(canvas);
+    return {
+      x: mousePos.x - canvasPos.x,
+      y: mousePos.y - canvasPos.y
+    };
+  }
 
-  if(mouseDown) {
-    // update the angles based on how far we moved since last time
-    angleY -= (mouse.x - oldX) * 0.01;
-    angleX += (mouse.y - oldY) * 0.01;
+  var mouseDown = false, oldX, oldY;
 
-    // don't go upside down
-    angleX = Math.max(angleX, -Math.PI / 2 + 0.01);
-    angleX = Math.min(angleX, Math.PI / 2 - 0.01);
-
-    // clear the sample buffer
-    ui.renderer.pathTracer.sampleCount = 0;
-
-    // remember this coordinate
+  document.onmousedown = function(event) {
+    var mouse = canvasMousePos(event);
     oldX = mouse.x;
     oldY = mouse.y;
-  } else {
-    var canvasPos = elementPos(canvas);
-    ui.mouseMove(mouse.x, mouse.y);
-  }
-};
 
-document.onmouseup = function(event) {
-  mouseDown = false;
+    if(mouse.x >= 0 && mouse.x < 512 && mouse.y >= 0 && mouse.y < 512) {
+      mouseDown = !ui.mouseDown(mouse.x, mouse.y);
 
-  var mouse = canvasMousePos(event);
-  ui.mouseUp(mouse.x, mouse.y);
-};
-
-document.onkeydown = function(event) {
-  // if there are no <input> elements focused
-  if(inputFocusCount == 0) {
-    // if backspace or delete was pressed
-    if(event.keyCode == 8 || event.keyCode == 46) {
-      ui.deleteSelection();
-
-      // don't let the backspace key go back a page
+      // disable selection because dragging is used for rotating the camera and moving objects
       return false;
     }
-  }
+
+    return true;
+  };
+
+  document.onmousemove = function(event) {
+    var mouse = canvasMousePos(event);
+
+    if(mouseDown) {
+      // update the angles based on how far we moved since last time
+      angleY -= (mouse.x - oldX) * 0.01;
+      angleX += (mouse.y - oldY) * 0.01;
+
+      // don't go upside down
+      angleX = Math.max(angleX, -Math.PI / 2 + 0.01);
+      angleX = Math.min(angleX, Math.PI / 2 - 0.01);
+
+      // clear the sample buffer
+      ui.renderer.pathTracer.sampleCount = 0;
+
+      // remember this coordinate
+      oldX = mouse.x;
+      oldY = mouse.y;
+    } else {
+      var canvasPos = elementPos(canvas);
+      ui.mouseMove(mouse.x, mouse.y);
+    }
+  };
+
+  document.onmouseup = function(event) {
+    mouseDown = false;
+
+    var mouse = canvasMousePos(event);
+    ui.mouseUp(mouse.x, mouse.y);
+  };
+
+  document.onkeydown = function(event) {
+    // if there are no <input> elements focused
+    if(inputFocusCount == 0) {
+      // if backspace or delete was pressed
+      if(event.keyCode == 8 || event.keyCode == 46) {
+        ui.deleteSelection();
+
+        // don't let the backspace key go back a page
+        return false;
+      }
+    }
+  };
+
 };
