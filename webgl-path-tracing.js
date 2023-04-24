7fe65d531e6fc85ddafa92e9895ab421826f56c1
Steren <steren.giannini@gmail.com>
2023-04-24 21:21:51 +0000

    fix glossiness

--- webgl-path-tracing.js
+++ webgl-path-tracing.js
@@ -1029,14 +1029,14 @@ function tick(timeSinceStart) {
  * Initialize the path tracer on the given canvas
  * @param {HTMLCanvasElement} canvas - Canvas to render to
  * @param {Object[]} objects - Array of Sphere and Cube objects
- * @param {Object} [configuration] - Specify material and environment to use
+ * @param {Object} [config] - Specify material and environment to use
  * @param {bool} [interactive=true] - if the user should be able to interact with the scene
  * @param {function} [log] - a function to print log messages to, defaults to console.log
  * @returns {UI}
  */
 function makePathTracer(canvas, objects, config = {}, interactive = true, log) {
-  material = config.material;
-  glossinessFactor = config.glossinessFactor;
+  material = config.material || material;
+  glossiness = config.glossiness || glossiness;
   environment = config.environment;
   nextObjectId = objects.length + 1;
 
